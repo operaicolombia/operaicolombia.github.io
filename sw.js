c2b3278682ff167bbc1327f0c6a04aee68988036
operaicolombia <operaicolombia@gmail.com>
2026-09-02 12:38:21 -0500

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'operai-colombia-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/OPERAICOL.jpeg',
+  '/fortunateSon.mp3'
+];
+
+// Instalar y guardar archivos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activar y limpiar caché viejo
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Servir archivos desde caché
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
